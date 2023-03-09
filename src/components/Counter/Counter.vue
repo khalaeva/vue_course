a74f2e97a78a845903b5219e4f3929126d0f3942
khalaeva <121284348+khalaeva@users.noreply.github.com>
2023-03-09 14:04:22 +0300

    button counter

---
+++ src/components/Counter/Counter.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import { reactive, ref } from "vue";
+
+    const counter = ref(0);
+
+    function counterHandler(event) {
+        console.log();
+    }
+</script>
+
+<template>
+    <button @click="counter++">counter {{ counter }}</button>
+</template>
+
+<style lang="scss" scoped></style>
